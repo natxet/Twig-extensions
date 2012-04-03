bd237c0bd60948967e9a4957673f1027fb94f586
natxet <nacho@cofre.net>
2012-04-03 16:52:12 +0200

    Adding Markdown

---
+++ lib/Twig/Extensions/Extension/Markdown.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!defined('ENT_SUBSTITUTE')) {
+    define('ENT_SUBSTITUTE', 8);
+}
+
+class Twig_Extensions_Extension_Markdown extends Twig_Extension
+{
+    public function getTokenParsers()
+    {
+        return array(
+            new Twig_Extensions_TokenParser_Markdown(),
+        );
+    }
+
+    public function getFilters()
+    {
+        $filters = array(
+            // formatting filters
+            'markdown'=> new Twig_Filter_Function('twig_markdown'),
+        );
+
+        return $filters;
+    }
+
+    public function getName()
+    {
+        return 'markdown';
+    }
+
+}
+
+function twig_markdown($data)
+{
+    include_once dirname(__FILE__).'/../Markdown/markdown.php';
+    return Markdown($data);
+}
+
---
+++ lib/Twig/Extensions/Node/Markdown.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Represents a markdown node.
+ *
+ * @package    twig
+ * @subpackage Twig-extensions
+ * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @author     Nacho Cofré, inspired by geta6 <getakura@gmail.com>
+ * @version    0.1
+ */
+class Twig_Extensions_Node_Markdown extends Twig_Node
+{
+    public function __construct(Twig_NodeInterface $body, $lineno, $tag = 'markdown')
+    {
+        parent::__construct(array('body' => $body), array(), $lineno, $tag);
+    }
+
+    /**
+     * Compiles the node to PHP.
+     *
+     * @param Twig_Compiler A Twig_Compiler instance
+     */
+    public function compile(Twig_Compiler $compiler)
+    {
+        include_once dirname(__FILE__).'/../Markdown/markdown.php';
+
+        $compiler
+            ->addDebugInfo($this)
+            ->write("\$string = <<< MARKDOWN\n")
+            ->write($this->getNode('body')->getAttribute('data'))
+            ->write("\nMARKDOWN;\n")
+	        ->write("echo Markdown(\$string);\n")
+        ;
+    }
+}
